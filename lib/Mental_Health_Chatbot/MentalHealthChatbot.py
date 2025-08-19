f410af2d800363d26de46bfb0924ddcae8493cb6
Dhanuka_Rathnayaka <dhanukarathnayaka658@gmail.com>
2025-08-19 13:01:34 +0530

    Changed-to-groq

--- lib/Mental_Health_Chatbot/MentalHealthChatbot.py
+++ lib/Mental_Health_Chatbot/MentalHealthChatbot.py
@@ -6,7 +6,14 @@ from groq import Groq
 from fastapi.middleware.cors import CORSMiddleware
 
 # Set your API keys
-GROQ_API_KEY = os.getenv("GROQ_API_KEY", "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")
+from dotenv import load_dotenv
+
+
+# Load variables from .env file
+load_dotenv()
+
+# Access your key
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 # Initialize Groq client
 groq_client = Groq(api_key=GROQ_API_KEY)
